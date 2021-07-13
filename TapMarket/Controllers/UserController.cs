196f377190e1a1c540a7650d35607b66f274d1d4
Bobby <boretobg@gmail.com>
2021-07-13 13:23:12 +0300

    Add profile view, very early phase

---
+++ TapMarket/Controllers/UserController.cs
@@ -0,0 +1,9 @@
+﻿namespace TapMarket.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserController : Controller
+    {
+        public IActionResult Profile() => View();
+    }
+}
