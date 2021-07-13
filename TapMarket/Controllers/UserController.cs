649934f7b672b6e7254ad41754a5cddc2999e9cc
Bobby <boretobg@gmail.com>
2021-07-13 14:20:16 +0300

    edit profile view, add listing list, edit buttons

--- TapMarket/Controllers/UserController.cs
+++ TapMarket/Controllers/UserController.cs
@@ -1,9 +1,30 @@
 ﻿namespace TapMarket.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+    using TapMarket.Data;
+    using TapMarket.Models.Listing;
 
     public class UserController : Controller
     {
-        public IActionResult Profile() => View();
+        private readonly TapMarketDbContext data;
+
+        public UserController(TapMarketDbContext data) => this.data = data;
+
+        public IActionResult Profile()
+        {
+            var listingsQuery = this.data.Listings.AsQueryable();
+
+            var listings = listingsQuery
+                .Select(l => new ListingViewModel
+                { 
+                    Title = l.Title,
+                    Price = l.Price,
+                    Condition = l.Condition,
+                    ImageUrl = l.ImageUrl
+                }).ToList();
+
+            return View(listings);
+        }
     }
 }
---
+++ TapMarket/Models/Listing/ListingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TapMarket.Models.Listing
+{
+    public class ListingViewModel
+    {
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Condition { get; set; }
+    }
+}
