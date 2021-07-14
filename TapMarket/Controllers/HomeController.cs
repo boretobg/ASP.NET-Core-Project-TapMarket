44fa2682ce96d6a3e9d05f05c7552ca7ba362d63
Bobby <boretobg@gmail.com>
2021-07-14 19:34:59 +0300

    add search bar and item listings in Index page (early stage)

--- TapMarket/Controllers/HomeController.cs
+++ TapMarket/Controllers/HomeController.cs
@@ -5,13 +5,34 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TapMarket.Data;
 using TapMarket.Models;
+using TapMarket.Models.Listing;
 
 namespace TapMarket.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly TapMarketDbContext data;
+
+        public HomeController(TapMarketDbContext data) 
+            => this.data = data;
+
+        public IActionResult Index()
+        {
+            var listingsQuery = this.data.Listings.AsQueryable();
+
+            var listings = listingsQuery
+                .Select(l => new ListingViewModel
+                {
+                    Title = l.Title,
+                    Price = l.Price,
+                    Condition = l.Condition,
+                    ImageUrl = l.ImageUrl
+                }).ToList();
+
+            return View(listings);
+        }
 
         public IActionResult About() => View();
 
