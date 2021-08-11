16fb54ed76de2f3d52404a271edae4193a590097
Bobby <boretobg@gmail.com>
2021-08-11 23:37:58 +0300

    add email sender service

---
+++ TapMarket/Services/Email/EmailService.cs
@@ -0,0 +1,40 @@
+﻿namespace TapMarket.Services
+{
+    using MailKit.Net.Smtp;
+    using MimeKit;
+
+    public class EmailService : IEmailService
+    {
+       public void SendEmail(
+           string senderName, 
+           string senderAddres,
+           string receiverName, 
+           string receiverAddres,
+           string subject, 
+           string content)
+        {
+            MimeMessage message = new MimeMessage();
+
+            MailboxAddress from = new MailboxAddress(senderName, senderAddres);
+            message.From.Add(from);
+
+            MailboxAddress to = new MailboxAddress(receiverName, receiverAddres);
+            message.To.Add(to);
+
+            message.Subject = subject;
+
+            BodyBuilder bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = content;
+
+            message.Body = bodyBuilder.ToMessageBody();
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp_gmail.com", 587, false);
+            client.Authenticate("user_name_here", "pwd_here"); //same as from
+
+            client.Send(message);
+            client.Disconnect(true);
+            client.Dispose();
+        }
+    }
+}
---
+++ TapMarket/Services/Email/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿namespace TapMarket.Services
+{
+    public interface IEmailService
+    {
+        public void SendEmail(
+           string senderName,
+           string senderAddres,
+           string receiverName,
+           string receiverAddres,
+           string subject,
+           string content);
+    }
+}
