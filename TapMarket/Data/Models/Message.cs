d45506fc6f275eae80343d1c897c7325100d89a3
Bobby <boretobg@gmail.com>
2021-08-16 20:51:40 +0300

    edit message table

--- TapMarket/Data/Models/Message.cs
+++ TapMarket/Data/Models/Message.cs
@@ -1,24 +1,19 @@
 ﻿namespace TapMarket.Data.Models
 {
-    using System;
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-
-    using static Data.DataConstants.Message;
 
     public class Message
     {
         public int Id { get; init; }
-
-        [Required]
-        [MaxLength(TextMaxLength)]
-        public string Text { get; set; }
-
-        public DateTime SentOn { get; set; }
 
         public string SenderId { get; set; }
         public User Sender { get; set; }
 
         public string ReceiverId { get; set; }
         public User Receiver { get; set; }
+
+        [Required]
+        public ICollection<MessageContent> Content { get; set; } = new List<MessageContent>();
     }
 }
