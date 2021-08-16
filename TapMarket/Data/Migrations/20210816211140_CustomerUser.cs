8c3e288b8e475285fbb7e298ea3e18947f1e3ee7
Bobby <boretobg@gmail.com>
2021-08-17 00:27:15 +0300

    add migrations

---
+++ TapMarket/Data/Migrations/20210816211140_CustomerUser.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TapMarket.Data.Migrations
+{
+    public partial class CustomerUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Favorites_AspNetUsers_CustomerId",
+                table: "Favorites");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerId",
+                table: "Favorites",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Favorites_CustomerId",
+                table: "Favorites",
+                newName: "IX_Favorites_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Favorites_AspNetUsers_UserId",
+                table: "Favorites",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Favorites_AspNetUsers_UserId",
+                table: "Favorites");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Favorites",
+                newName: "CustomerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Favorites_UserId",
+                table: "Favorites",
+                newName: "IX_Favorites_CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Favorites_AspNetUsers_CustomerId",
+                table: "Favorites",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
