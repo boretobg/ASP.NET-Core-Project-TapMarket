406fe8264d6c5171f0d1b313f02b423b03473f9d
Bobby <boretobg@gmail.com>
2021-08-18 23:50:40 +0300

    push workflow

---
+++ TapMarket.Test/Controller/ListingControllerTest.cs
@@ -0,0 +1,37 @@
+﻿namespace TapMarket.Test.Controller
+{
+    using Microsoft.AspNetCore.Mvc;
+    using TapMarket.Controllers;
+    using Xunit;
+
+    public class ListingControllerTest
+    {
+        [Fact]
+        public void CreatedListingShouldReturnView()
+        {
+            //Arrange
+            var listingController = new ListingController(null, null, null);
+
+            //Act
+            var result = listingController.CreatedListing();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void AddShouldReturnRedirect()
+        {
+            //Arrange
+            var listingController = new ListingController(null, null, null);
+
+            //Act
+            var result = listingController.Add();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<ViewComponentResult>(result);
+        }
+    }
+}
